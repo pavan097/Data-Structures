d45e94e6ea8edce022def71da6dc3e0939ba3755
pa1 <pavan970535@gmail.com>
2018-10-17 23:14:52 +0530

    Update hashtable.py

--- praticeProg/hashtable.py
+++ praticeProg/hashtable.py
@@ -1,10 +1,17 @@
-lst=[1,2,3]
+# lst=[1,2,3]
+class  Hash():
+    def insert(self,lst)
+        size_of_table=10
+        hash_table = [list() for _ in range(size_of_table)]
 
-size_of_table=10
-hash_table = [list() for _ in range(size)]
+        for i in range(len(lst)):
+            index=int(i%size_of_table)
+            ht[index]=a[i]
 
-for i in range(len(lst)):
-    index=int(i%size_of_table)
-    ht[index]=a[i]
-    
-print(hash_table)
+        print(hash_table)
+       
+    def search(self,value):
+        if value in hash_table:
+            print('value is present in the hash table')
+        else:
+            print('value is not in hash table')
